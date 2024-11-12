f28c600236a0ba56e9c44d193e7445ada79de3bc
Nguyễn Hữu Lâm <48357664+huulamthcsyl@users.noreply.github.com>
2024-11-12 23:43:48 +0700

    feat: sign up

--- lib/auth/models/sign_up_data.dart
+++ lib/auth/models/sign_up_data.dart
@@ -1,13 +1,37 @@
+import 'dart:convert';
+
+SignUpData signUpDataFromJson(String str) => SignUpData.fromJson(json.decode(str));
+
+String signUpDataToJson(SignUpData data) => json.encode(data.toJson());
+
 class SignUpData {
+  String? ho;
+  String? ten;
   String? email;
   String? password;
-  String? name;
   String? role;
 
   SignUpData({
+    this.ho,
+    this.ten,
     this.email,
     this.password,
-    this.name,
     this.role,
   });
+
+  factory SignUpData.fromJson(Map<String, dynamic> json) => SignUpData(
+    ho: json["ho"],
+    ten: json["ten"],
+    email: json["email"],
+    password: json["password"],
+    role: json["role"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "ho": ho,
+    "ten": ten,
+    "email": email,
+    "password": password,
+    "role": role,
+  };
 }
--- lib/auth/repositories/auth_respository.dart
+++ lib/auth/repositories/auth_respository.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
 import 'package:it4788_20241/auth/models/login_data.dart';
+import 'package:it4788_20241/auth/models/sign_up_data.dart';
 import 'package:it4788_20241/auth/models/user_data.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:it4788_20241/auth/models/verify_data.dart';
 import 'package:it4788_20241/const/api.dart';
+import 'package:it4788_20241/exceptions/GlobalException.dart';
 
 class AuthRepository {
   Future<UserData> login(LoginData loginData) async {
@@ -14,9 +17,48 @@ class AuthRepository {
     });
     if(response.statusCode == 200) {
       final body = jsonDecode(response.body);
-      return UserData.fromJson(body['data']);
+      if (body['status_code'] == 1000) {
+        return UserData.fromJson(body['data']);
+      } else {
+        throw GlobalException(body['message']);
+      }
     } else {
-      throw Exception("Tài khoản hoặc mật khẩu không đúng");
+      throw GlobalException("Tài khoản hoặc mật khẩu không đúng");
+    }
+  }
+
+  Future<void> signUp(SignUpData signUpData) async {
+    final httpUrl = Uri.http(BASE_API_URL, '/it4788/signup');
+    final response = await http.post(httpUrl, body: jsonEncode(signUpData.toJson()), headers: {
+      'Content-Type': 'application/json',
+    });
+    final body = jsonDecode(response.body);
+    if(response.statusCode == 200) {
+      if (body['status_code'] == 1000) {
+        final verifyData = VerifyData(email: signUpData.email, verifyCode: body['verify_code']);
+        verifyAccount(verifyData);
+      } else {
+        throw GlobalException(body['message']);
+      }
+    } else {
+      throw GlobalException(body['message']);
+    }
+  }
+
+  Future<void> verifyAccount(VerifyData verifyData) async {
+    final httpUrl = Uri.http(BASE_API_URL, '/it4788/check_verify_code');
+    final response = await http.post(httpUrl, body: jsonEncode(verifyData.toJson()), headers: {
+      'Content-Type': 'application/json',
+    });
+    final body = jsonDecode(response.body);
+    if(response.statusCode == 200) {
+      if (body['code'] == 1000) {
+        return;
+      } else {
+        throw GlobalException(body['message']);
+      }
+    } else {
+      throw GlobalException(body['message']);
     }
   }
 }
