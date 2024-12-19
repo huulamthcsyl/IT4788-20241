4feb3e5ddd3b1166dc22e818a5cb1cb27554824e
huulamthcsyl <huulamthcsyl@gmail.com>
2024-12-20 00:18:29 +0700

    feat: chat avatar

--- lib/chat/viewmodels/chat_overview_viewmodel.dart
+++ lib/chat/viewmodels/chat_overview_viewmodel.dart
@@ -45,6 +45,16 @@ class ChatOverviewViewModel extends ChangeNotifier {
     pagingController.refresh();
   }
 
+  String convertGoogleDriveLink(String link) {
+    final regex = RegExp(r'd/([a-zA-Z0-9_-]+)/view');
+    final match = regex.firstMatch(link);
+    if (match != null && match.groupCount >= 1) {
+      final fileId = match.group(1);
+      return 'https://drive.google.com/uc?export=view&id=$fileId';
+    }
+    return link;
+  }
+
   Future<List<SearchResult>> getSearchResult(String searchContent) {
     return _searchService.getSearchResult(searchContent, "0", "5");
   }
